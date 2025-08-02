5d51de5122cfa1dfb96ddf90f69dde693be24b50
ultimate3101 <139368417+ultimate3101@users.noreply.github.com>
2025-08-02 14:17:45 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from Koyeb!'));
+app.get('/health', (req, res) => res.status(200).send('OK'));
+
+app.listen(port, () => console.log(`Running on port ${port}`));
